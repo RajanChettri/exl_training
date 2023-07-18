f234e7f3b1c4b20dc590aab1ab7d0b5a9452722a
RajanChettri <rajan_chettri@live.com>
2023-07-18 12:38:06 +0530

    Program Uploaded by Rajan

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day5
+{
+
+    // Inheritance 
+    // What is Inheritance ?
+    // Inheritance Refers the Practice of Acquiring the Members of Base class in 
+    // derived Class
+
+    class Father
+    {
+       public void foodHabit()
+        {
+            Console.WriteLine("I Like Salty");
+        }
+    }
+    class Son : Father
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Son gopal = new Son();
+            gopal.foodHabit();
+        }
+    }
+}
